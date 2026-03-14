9c4ef76e2d203515e17c74b5fcdbff985aa565a6
jomar1231 <vergarajomar222@gmail.com>
2026-03-14 09:34:28 +0800

    backend register

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -26,7 +26,7 @@ exports.login = (req,res)=>{
     const valid = await bcrypt.compare(password, user.password);
     if(!valid) return res.status(401).json({message:"Wrong password"});
 
-    const token = jwt.sign({id:user.id,email:user.email},"secretkey",{expiresIn:"1h"});
+    const token = jwt.sign({id:user.id,email:user.email},process.env.JWT_SECRET || "secretkey",{expiresIn:"1h"});
     res.json({message:"Login successful", token});
   });
 };
--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -2,7 +2,7 @@ const db = require('../config/database');
 
 
 exports.createUser = (user,callback) =>{
-    const {firstname,lastname,email,Password} = user;
+    const {firstname,lastname,email,password} = user;
     db.query("INSERT INTO register (First_Name,Last_Name, Email, Password) VALUES (?,?,?,?)",
         [firstname,lastname,email,Password],callback
     );
